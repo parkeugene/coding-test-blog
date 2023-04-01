15ac934c2961c435d061e46bb387965d9bd5c9cc
hyejj19 <hyejj19@naver.com>
2023-04-01 20:03:06 +0900

    style : BlogPage 생성 및 Router 경로 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MainPage from '../pages/MainPage';
+import BlogPage from '../pages/BlogPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/blog" element={<BlogPage />} />
       </Routes>
     </BrowserRouter>
   );
