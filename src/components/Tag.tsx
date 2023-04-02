c3430a877b367afa6f0d6faa38a5f01cbd9683ba
hyejj19 <hyejj19@naver.com>
2023-04-02 10:56:47 +0900

    feat : tags Props 토대로 렌더링하도록 변경

--- src/components/Tag.tsx
+++
@@ -1,9 +0,0 @@
-const Tag = () => {
-  return (
-    <span className="mb-5 inline-block rounded-full bg-green-light px-2 py-1 font-body text-sm text-green sm:mb-8">
-      category
-    </span>
-  );
-};
-
-export default Tag;
