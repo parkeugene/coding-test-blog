fad5c4df290a0fb92ce9dccd0613bae7ad0972df
hyejj19 <hyejj19@naver.com>
2023-04-02 10:13:36 +0900

    modify : 블로그 리스트 api 요청 limit 추가

--- src/api/blogApi.ts
+++ src/api/blogApi.ts
@@ -7,17 +7,19 @@ export const blogApi = {
       collection: 'posts',
       database: 'sample_training',
       dataSource: 'Cluster0',
+      limit: 10,
     });
 
     try {
       const response = await httpClient.post<BlogListResp>('/api/app/data-lhvnl/endpoint/data/v1/action/find', payload);
 
-      return response.data;
+      return response.data.documents;
     } catch (error) {
       console.error(error);
       throw new Error('블로그 리스트 가져오기 요청 실패!');
     }
   },
+
   getBlogDetail() {
     console.log('blogDetail');
   },
