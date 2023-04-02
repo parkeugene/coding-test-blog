40d7ca3ed4d97b86f7da0f455afb88fb5244f1d4
hyejj19 <hyejj19@naver.com>
2023-04-02 10:15:28 +0900

    feat : 블로그 데이터 가져오기 및 화면 렌더링
    * props 전달하기

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,13 +1,28 @@
 import PostListItem from './PostListItem';
+import { useFetch } from '../hooks/useFetch';
+import { BlogPostResp } from '../types/types';
+import LoadingSpinner from './LoadingSpinner';
 
 const PostList = () => {
+  const { data: blogList, isLoading, isError } = useFetch('getBlogList');
+
+  if (isLoading) {
+    return (
+      <div className="pt-8 py-10">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div className="pt-8 py-10">블로그 데이터 로드에 실패하였습니다.</div>;
+  }
+
   return (
     <div className="pt-8">
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
+      {blogList.map((blog: BlogPostResp) => (
+        <PostListItem key={blog.date} />
+      ))}
     </div>
   );
 };
