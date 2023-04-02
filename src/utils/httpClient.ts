5fb3a56dfc000ab056fecb8e5eecdf5f1cafe9f7
hyejj19 <hyejj19@naver.com>
2023-04-02 09:16:34 +0900

    feat : proxy 우회하여 cors issue 해결

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URI,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Request-Headers': '*',
     'api-key': import.meta.env.VITE_API_KEY,
   },
 });
