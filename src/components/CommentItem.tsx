2327969803075f0c58f1b172f26f0cd0496bbe76
hyejj19 <hyejj19@naver.com>
2023-04-03 04:45:41 +0900

    refactor : 코멘트 컴포넌트 분리 / 코멘트 페이지네이션 구현

---
+++ src/components/CommentItem.tsx
@@ -0,0 +1,12 @@
+const CommentItem = () => {
+  return (
+    <div className="text-sm border border-grey-lighter">
+      <div className="bg-grey-lightest px-3 py-2 font-bold">
+        <span>Houston Valenti</span>
+      </div>
+      <div className="px-3 py-5">Lorem ipsum dolor sit amet, consectetur</div>
+    </div>
+  );
+};
+
+export default CommentItem;
