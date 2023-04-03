1b49116dfd72c70c2170bb219dfa0b83c1b857e2
hyejj19 <hyejj19@naver.com>
2023-04-03 13:05:45 +0900

    style : 코멘트 다크모드 클래스 추가

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -4,7 +4,7 @@ const CommentItem = () => {
       <div className="bg-grey-lightest px-3 py-2 font-bold">
         <span>Houston Valenti</span>
       </div>
-      <div className="px-3 py-5">Lorem ipsum dolor sit amet, consectetur</div>
+      <div className="px-3 py-5 dark:text-white">Lorem ipsum dolor sit amet, consectetur</div>
     </div>
   );
 };
